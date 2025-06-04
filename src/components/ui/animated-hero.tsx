2000285ae74f0af2f1e7b21d17ad813502240365
smammar100 <mrammarbest110@gmail.com>
2025-06-04 21:52:44 +0500

    Updated animated-hero.tsx

--- src/components/ui/animated-hero.tsx
+++ src/components/ui/animated-hero.tsx
@@ -25,7 +25,7 @@ function Hero() {
   return (
     <div className="w-full">
       <div className="container mx-auto">
-        <div className="flex gap-8 py-20 lg:py-40 items-center justify-center flex-col">
+        <div className="flex gap-8 py-10 lg:py-20 items-center justify-center flex-col">
           <AnimatedShinyTextDemo />
           <div className="flex gap-4 flex-col">
             <h1 className="text-5xl md:text-7xl max-w-2xl tracking-tighter text-center font-regular">
