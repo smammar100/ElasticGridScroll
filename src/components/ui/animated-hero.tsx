8416687ca338aa110ffd90f6f8391f274d822446
smammar100 <mrammarbest110@gmail.com>
2025-06-04 21:43:03 +0500

    Updated animated-hero.tsx

--- src/components/ui/animated-hero.tsx
+++ src/components/ui/animated-hero.tsx
@@ -63,14 +63,6 @@ function Hero() {
               ever.
             </p>
           </div>
-          <div className="flex flex-row gap-3">
-            <Button size="lg" className="gap-4" variant="outline">
-              Jump on a call <PhoneCall className="w-4 h-4" />
-            </Button>
-            <Button size="lg" className="gap-4">
-              Sign up here <MoveRight className="w-4 h-4" />
-            </Button>
-          </div>
         </div>
       </div>
     </div>
